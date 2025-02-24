bfb61ef2684304c3775f1b418eb32721d68cfe0a
Karma Tamang <tamangkarma160@gmail.com>
2025-02-24 19:08:29 +0545

    hero-section

---
+++ app.js
@@ -0,0 +1,7 @@
+const left = document.querySelector('.left-btn');
+const right = document.querySelector('.right-btn');
+const slider = document.querySelector('.slider');
+
+right.addEventListener('click', () => {
+  
+});
